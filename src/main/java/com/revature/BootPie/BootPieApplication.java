b50c3a5eed55ca21247c05360e5d5ae5cc575a57
HazemMasarani <hazemmasarani99@gmail.com>
2025-07-25 20:08:56 -0400

    project main

---
+++ src/main/java/com/revature/BootPie/BootPieApplication.java
@@ -0,0 +1,13 @@
+package com.revature.BootPie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/BootPie/BootPieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.BootPie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
